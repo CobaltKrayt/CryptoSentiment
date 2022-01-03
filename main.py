99656c504f7db0013de5e18f208c347f2e00306b
CobaltKrayt <57502696+CobaltKrayt@users.noreply.github.com>
2022-01-03 22:07:46 +0200

    Twitter Scraper v0.1

--- main.py
+++ main.py
@@ -1,63 +1,19 @@
-from pprint import pprint
-import pandas as pd
-import nltk
-import praw
-import seaborn as sns
-from matplotlib import pyplot as plt
-import pytz
+import csv
+from getpass import getpass
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# time format
-# est = pytz.timezone('US/Eastern')
-# utc = pytz.utc
-# fmt = '%d-%m-%Y %H:%M:%S %Z%z'
 
-nltk.download('vader_lexicon')
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
 
-user_agent = "ConfusedScraper 0.1 by /u/CobaltDoofus"
-reddit = praw.Reddit(
-    client_id="q-47jXqN4JQs6XBMWyzsWw",
-    client_secret="-U6J92hdsT8vTD2L9kMGWLqiN46AJg",
-    user_agent=user_agent
-)
+wait = WebDriverWait(driver, 30)
+driver.get("https://www.twitter.com/login")
 
-headlines = set()
-for submission in reddit.subreddit('CryptoCurrency').hot(limit=None):
-    headlines.add((submission.title ,submission.created_utc))
-
-print(len(headlines))
-
-df = pd.DataFrame(headlines)
-df.to_csv('redditHeadlines.csv',header=False,encoding='utf-8', index=False)
-
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-
-sia = SIA()
-results=[]
-
-for line in headlines:
-    pol_score = sia.polarity_scores(line[0])
-    pol_score['headline'] = line[0]
-    pol_score['date'] = line[1]
-    results.append(pol_score)
-
-# pprint(results[:3],width=100)
-
-df = pd.DataFrame.from_records(results)
-
-df['label'] = 0
-df.loc[df['compound'] > 0.2,'label'] = 1
-df.loc[df['compound'] < -0.2,'label'] = -1
-
-df.head()
-
-df.to_csv('redditHeadlinesWLabels.csv',header=False,encoding='utf-8', index=False)
-
-fix,ax = plt.subplots(figsize=(8,8))
-counts = df.label.value_counts(normalize=True)*100
-
-sns.barplot(x=counts.index,y=counts,ax=ax)
-
-ax.set_xlabel(['Negative','Neutral','Positive'])
-ax.set_ylabel("Percentage")
-
-plt.show()
+username = wait.until(EC.visibility_of_element_located((By.NAME, "text")))
+username.send_keys('George Covalciuc')
