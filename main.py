40a22202072225c47b740cc3104393313a3d4ff9
CobaltKrayt <57502696+CobaltKrayt@users.noreply.github.com>
2022-01-04 15:33:25 +0200

    Twitter Scraper v0.2(NF) - added full login ability, search query input. WIP: Post scraping

--- main.py
+++ main.py
@@ -16,4 +16,23 @@ wait = WebDriverWait(driver, 30)
 driver.get("https://www.twitter.com/login")
 
 username = wait.until(EC.visibility_of_element_located((By.NAME, "text")))
-username.send_keys('George Covalciuc')
+username.send_keys('GmanCC')
+
+username.send_keys(Keys.RETURN)
+
+password = wait.until(EC.visibility_of_element_located((By.NAME,"password")))
+password.send_keys('Georgia804bot')
+
+password.send_keys(Keys.RETURN)
+
+search = wait.until(EC.visibility_of_element_located((By.XPATH,'//input[@aria-label="Search query"]')))
+search.send_keys('#cryptocurrency')
+search.send_keys(Keys.RETURN)
+
+cards = wait.until(EC.visibility_of_all_elements_located((By.XPATH,'//article[@data-testid="tweet"]')))
+card = cards[0]
+
+# username
+
+print(card.find_element(By.XPATH,'./div[2]/div[1]//span').text)
+
