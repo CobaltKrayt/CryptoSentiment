c2d51af172d9ecdd23071ae5510fb6168de9a821
CobaltKrayt <57502696+CobaltKrayt@users.noreply.github.com>
2022-01-08 17:28:51 +0200

    Twitter Scraper v0.4.01 - added complete post scraping

--- main.py
+++ main.py
@@ -35,23 +35,26 @@ card = cards[0]
 # username
 # ./div/div/div/div[2]/div[2]/div[1]//span
 
-username = card.find_element(By.XPATH,'.//span').text
-userTag = card.find_element(By.XPATH,'.//span[contains(text(),"@")]').text
-postTime = card.find_element(By.XPATH,'.//time').get_attribute('datetime')
-tweetContent = card.find_element(By.XPATH,'./div/div/div/div[2]/div[2]/div[2]/div[1]').text
-respondingTo = card.find_element(By.XPATH,'./div/div/div/div[2]/div[2]/div[2]/div[2]').text
-
-replyCount = card.find_element(By.XPATH,'.//div[@data-testid="reply"]').text
-retweetCount = card.find_element(By.XPATH,'.//div[@data-testid="retweet"]').text
-likeCount = card.find_element(By.XPATH,'.//div[@data-testid="like"]').text
 
 
-print(username)
-print(userTag)
-print(postTime)
-print(tweetContent)
-print(respondingTo)
-print(replyCount)
-print(retweetCount)
-print(likeCount)
+def getTweetData(card):
+    try:
+        postTime = card.find_element(By.XPATH, './/time').get_attribute('datetime')
+    except NoSuchElementException:
+        return
+
+    username = card.find_element(By.XPATH, './/span').text
+    userHandle = card.find_element(By.XPATH, './/span[contains(text(),"@")]').text
+    tweetContent = card.find_element(By.XPATH, './div/div/div/div[2]/div[2]/div[2]/div[1]').text
+    respondingTo = card.find_element(By.XPATH, './div/div/div/div[2]/div[2]/div[2]/div[2]').text
+
+    replyCount = card.find_element(By.XPATH, './/div[@data-testid="reply"]').text
+    retweetCount = card.find_element(By.XPATH, './/div[@data-testid="retweet"]').text
+    likeCount = card.find_element(By.XPATH, './/div[@data-testid="like"]').text
+
+    tweet = (username, userHandle, postTime,tweetContent,respondingTo,replyCount,retweetCount,likeCount)
+    return tweet
+
+print(getTweetData(card))
+
 
